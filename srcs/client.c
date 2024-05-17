e95837c75f9c61a5990a76740a809a85767b31b6
Barragues <petrouch@informatique.univ-paris-diderot.fr>
2024-05-17 19:11:55 +0200

    modif multidiffusion

--- srcs/client.c
+++ srcs/client.c
@@ -63,7 +63,7 @@ int main (int argc, const char *argv[]) {
         paquets_envoyes += res_send;  
     }
 
-   u_int16_t reponse_serveur[4];
+    u_int16_t reponse_serveur[11];
          
     /* Attente de la réponse  */
     while ((size_t)octets_recu < sizeof(reponse_serveur)) {
@@ -95,7 +95,7 @@ int main (int argc, const char *argv[]) {
         perror("Erreur lors de la conversion de l'adresse IP");
         return -1;
     }
-    servadr_dest.sin6_port = portUDP;
+    servadr_dest.sin6_port = htons(portUDP);
 
     char buf[25];
     sprintf(buf, "coucou ça fonctionne !");
@@ -109,56 +109,70 @@ int main (int argc, const char *argv[]) {
 void abonnementMultidiff (u_int16_t portMDIFF, u_int16_t reponse_serveur[]){
     
     /* le client doit s'abonner à l'adresseMultiDiff de multidiffusion */
-    int sockUDP = socket(PF_INET6, SOCK_DGRAM,0);
+    int sockUDP = socket(AF_INET6, SOCK_DGRAM,0);
     if (sockUDP < 0){ 
         perror("Socket UDP");
         exit(EXIT_FAILURE);
     }
-    
+
+    // Reconstituer l'adresse IPv6 à partir de rep
     struct sockaddr_in6 adresseMultiDiff;
     memset(&adresseMultiDiff, 0, sizeof(adresseMultiDiff));
     adresseMultiDiff.sin6_family = AF_INET6;
     adresseMultiDiff.sin6_addr = in6addr_any;
+    for (int i = 0; i < 11; ++i) {
+        adresseMultiDiff.sin6_addr.s6_addr[i * 2] = (reponse_serveur[i+3] >> 8) & 0xFF;
+        adresseMultiDiff.sin6_addr.s6_addr[i * 2 + 1] = reponse_serveur[i+3] & 0xFF;
+    }
     adresseMultiDiff.sin6_port = htons(portMDIFF);
-    memcpy(adresseMultiDiff.sin6_addr.s6_addr,reponse_serveur+3,sizeof(adresseMultiDiff.sin6_addr.s6_addr));
-   
+
+    // Affichage pour vérifier l'adresse de multidiffusion
     char adresse_str[INET6_ADDRSTRLEN];
     if (inet_ntop(AF_INET6, &adresseMultiDiff.sin6_addr, adresse_str, INET6_ADDRSTRLEN) == NULL) {
         perror("inet_ntop");
-        exit(EXIT_FAILURE);
-    }
-    printf("Adresse de multidiffusion : %s\n", adresse_str);
-    
-    /* liaison de la socket au port pour permettre la réception des paquets */
-    if(bind(sockUDP, (struct sockaddr*)&adresseMultiDiff, sizeof(adresseMultiDiff))) {
-        perror("bind");
         close(sockUDP);
-        exit(EXIT_FAILURE);
-    }   
+    }
+    printf("Adresse de multidiffusion client : %s\n", adresse_str);
+   
+    /* Lier la socket à toute interface et au port de multidiffusion*/
+    struct sockaddr_in6 localAddr;
+    memset(&localAddr, 0, sizeof(localAddr));
+    localAddr.sin6_family = AF_INET6;
+    localAddr.sin6_addr = in6addr_any;
+    localAddr.sin6_port = htons(portMDIFF); 
 
+    if (bind(sockUDP, (struct sockaddr*)&localAddr, sizeof(localAddr)) < 0) {
+        perror("Erreur lors de la liaison de la socket");
+        close(sockUDP);
+    }
+    printf("portMDIFF abonnement = %u\n", portMDIFF);
+    
     /* abonnement de l'entité au groupe multicast */
     struct ipv6_mreq group;
     memcpy(&group.ipv6mr_multiaddr, &adresseMultiDiff.sin6_addr, sizeof(struct in6_addr));
-    group.ipv6mr_interface = if_nametoindex("wlp0s20f3"); /* interface réseau multicast sur ma machine */
-    
-    if(setsockopt(sockUDP, IPPROTO_IPV6, IPV6_JOIN_GROUP, &group, sizeof(group)) < 0){
+    group.ipv6mr_interface = if_nametoindex("eth0"); /* interface réseau multicast par défaut */
+       
+    if( setsockopt(sockUDP, IPPROTO_IPV6, IPV6_JOIN_GROUP, &group, sizeof(group)) < 0){
         perror("setsockopt");
         close(sockUDP);
         exit(EXIT_FAILURE);
     }
-
     printf("abonnement OK\n");
-    close(sockUDP);
     
-    /* lecture des messages multicast diffusé par le serveur *
-    while (1){    
-        ssize_t paquet_recu = read(sockUDP, buf, MAX_BUF);
-        if (paquet_recu < 0){
-            perror("erreur recvfrom");
-            close(sockUDP);
-            exit(EXIT_FAILURE);
-        }
+    char buf[1024];
+    ssize_t paquet_recu;
+    struct sockaddr_in6 expediteur;
+    socklen_t addrlen = sizeof(expediteur);
 
+    /* Lecture des messages multicast diffusés par le serveur */
+    while (1){    
+        paquet_recu = recvfrom(sockUDP, buf, sizeof(buf),0,(struct sockaddr *)&expediteur, &addrlen);
+        if (paquet_recu < 0){
+            perror("Erreur lors de la réception du message");
+            close(sockUDP);
+        }
         printf("Message reçu du serveur: %.*s\n", (int)paquet_recu, buf);
-    }*/
+    }
+
+    close(sockUDP);
 }
--- srcs/serveur_partie.c
+++ srcs/serveur_partie.c
@@ -76,7 +76,7 @@ int rajoute_joueur_partie(liste_parties_t *lp, int type) // rajouter un gros loc
             servadr.sin6_addr = in6addr_any;
             servadr.sin6_port = htons(PORT_UDP);
             if (bind(sock_serv_UDP, (struct sockaddr *)&servadr, sizeof(servadr)) < 0) return -1;
-        printf("bbbb\n");
+            printf("bbbb\n");
 
             partie_t *p = malloc(sizeof(partie_t));
             j->id=0;
@@ -132,7 +132,7 @@ int client_thread(arg_thread_t *args)
 
     u_int16_t rep_0 = 0;
 
-    u_int16_t rep[4];
+    u_int16_t rep[11];
     memset(&rep, 0, sizeof(u_int16_t)*4);
 
 
@@ -160,7 +160,6 @@ int client_thread(arg_thread_t *args)
     rep_0 |= (u_int16_t)((id & 0x3) << 13);
     rep_0 |= (u_int16_t)((1 & 0x1) << 15);
     rep[0] = htons(rep_0);
-
     rep[1] = htons(PORT_UDP);
     rep[2] = htons(PORT_MDIF);
     
@@ -169,8 +168,13 @@ int client_thread(arg_thread_t *args)
     adresseMultiDiff.sin6_family = AF_INET6;
     inet_pton(AF_INET6,"ff12::1:2:3", &adresseMultiDiff.sin6_addr);
     adresseMultiDiff.sin6_port = htons(PORT_MDIF);
-    memcpy(&rep[3], adresseMultiDiff.sin6_addr.s6_addr, sizeof(rep[3]));
-   
+    printf("portMDIFF serveur = %u", PORT_MDIF);
+
+    // Copiel'adresse IPv6 en segments de 2 octets dans rep
+    for (int i = 0; i < 11 ;++i) {
+        rep[i+3] = (adresseMultiDiff.sin6_addr.s6_addr[i * 2] << 8) | adresseMultiDiff.sin6_addr.s6_addr[i * 2 + 1];
+    }
+
     serveur(adresseMultiDiff);
 
     int reponse = 0;
@@ -197,8 +201,8 @@ int client_thread(arg_thread_t *args)
     return 1;
 }
 
-int serveur(struct sockaddr_in6 addr_server) {
-    
+int serveur(struct sockaddr_in6 adresseMultiDiff) {
+
     /* déclaration d'une socket UDP IPv6 */
     int sock = socket(PF_INET6, SOCK_DGRAM,0); 
     if (sock < 0){
@@ -207,7 +211,7 @@ int serveur(struct sockaddr_in6 addr_server) {
     }
 
     /* Liaison de la socket à une interface réseau spécifique */
-    int ifindex = if_nametoindex("wlp0s20f3"); /* interface réseau multicast sur ma machine */
+    int ifindex = if_nametoindex("eth0"); /* interface réseau multicast sur ma machine */
     if (ifindex == 0) {
         perror("Erreur lors de la récupération de l'index de l'interface");
         close(sock);
@@ -219,13 +223,23 @@ int serveur(struct sockaddr_in6 addr_server) {
         exit(EXIT_FAILURE);
     }
 
-    /* Liaison de la socket au port */
-    if (bind(sock, (struct sockaddr *)&addr_server, sizeof(addr_server)) < 0) {
-        perror("Erreur lors de la liaison de la socket au port");
+    char adresse_str[INET6_ADDRSTRLEN];
+    if (inet_ntop(AF_INET6, &adresseMultiDiff.sin6_addr, adresse_str, INET6_ADDRSTRLEN) == NULL) {
+        perror("inet_ntop");
         exit(EXIT_FAILURE);
     }
+    //printf("Adresse de multidiffusion coté serveur 2 : %s\n", adresse_str);
+
     printf("diffusion OK\n");
 
- 
+    char message[] = "Message de test";
+    ssize_t tailleEnvoie = sendto(sock, message, strlen(message), 0, (struct sockaddr *)&adresseMultiDiff, sizeof(adresseMultiDiff));
+    if (tailleEnvoie < 0) {
+        perror("Erreur lors de l'envoi du message");
+        close(sock);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Envoie OK\n");
     return 0;
 }
